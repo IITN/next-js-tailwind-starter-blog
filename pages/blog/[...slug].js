acfbdf8fa9ebd326ef530a9187e1a66390349f4e
erikuusitalo <erik@uusitalo.io>
2022-03-03 15:31:06 +0100

    log frontmatter

--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -54,7 +54,7 @@ export async function getStaticProps(context) {
 export default function Blog({ authorDetails, prev, next, page }) {
   const { contentHtml, frontMatter } = page;
 
-  console.log("PAGE", page);
+  console.log("frontMatter", frontMatter);
 
   return (
     <PostLayout
