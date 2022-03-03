627efa70b725d0a92ba5fdc361c44c54fe097570
erikuusitalo <erik@uusitalo.io>
2022-03-03 12:02:25 +0100

    remove collection

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import Tag from "@/components/Tag";
 import siteMetadata from "@/data/siteMetadata";
 import { getAllFilesFrontMatter } from "@/lib/mdx";
 import formatDate from "@/lib/utils/formatDate";
-import { getCollection } from "../lib/collection";
 
 import NewsletterForm from "@/components/NewsletterForm";
 
