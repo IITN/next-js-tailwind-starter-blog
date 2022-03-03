31ec954cfc16945b127648b068c428b86a8e2138
erikuusitalo <erik@uusitalo.io>
2022-03-03 14:25:50 +0100

    log page props

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,8 @@ export default function App({ Component, pageProps }) {
     },
   });
 
+  console.log("pageProps: ", pageProps);
+
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
       <Head>
