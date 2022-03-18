8a0e4806554ca22ec387f12834eb542313c559d9
erikuusitalo <erik@uusitalo.io>
2022-03-18 15:38:45 +0100

    fix _ data

--- pages/blog/page/[page].js
+++ pages/blog/page/[page].js
@@ -1,50 +1,71 @@
-import { PageSEO } from '@/components/SEO'
-import siteMetadata from '@/data/siteMetadata'
-import { getAllFilesFrontMatter } from '@/lib/mdx'
-import ListLayout from '@/layouts/ListLayout'
-import { POSTS_PER_PAGE } from '../../blog'
+import { PageSEO } from "@/components/SEO";
+import siteMetadata from "@/data/siteMetadata";
+import { getAllFilesFrontMatter, pathToSlug, getFileBySlug } from "@/lib/mdx";
+import ListLayout from "@/layouts/ListLayout";
+import { POSTS_PER_PAGE } from "../../blog";
 
 export async function getStaticPaths() {
-  const totalPosts = await getAllFilesFrontMatter('blog')
-  const totalPages = Math.ceil(totalPosts.length / POSTS_PER_PAGE)
+  const totalPosts = await getAllFilesFrontMatter("blog");
+  const totalPages = Math.ceil(totalPosts.length / POSTS_PER_PAGE);
   const paths = Array.from({ length: totalPages }, (_, i) => ({
     params: { page: (i + 1).toString() },
-  }))
+  }));
 
   return {
     paths,
     fallback: false,
-  }
+  };
 }
 
 export async function getStaticProps(context) {
   const {
     params: { page },
-  } = context
-  const posts = await getAllFilesFrontMatter('blog')
-  const pageNumber = parseInt(page)
-  const initialDisplayPosts = posts.slice(
+  } = context;
+  const posts = await getAllFilesFrontMatter("blog");
+
+  const postsWithTags = await Promise.all(
+    posts.map(async (post) => {
+      const tags = await Promise.all(
+        post.tags.map(async (path) => {
+          const slug = pathToSlug(path);
+          const tag = await getFileBySlug("tag", slug);
+          return tag.frontMatter;
+        })
+      );
+
+      return {
+        ...post,
+        tags,
+      };
+    })
+  );
+
+  const pageNumber = parseInt(page);
+  const initialDisplayPosts = postsWithTags.slice(
     POSTS_PER_PAGE * (pageNumber - 1),
     POSTS_PER_PAGE * pageNumber
-  )
+  );
   const pagination = {
     currentPage: pageNumber,
-    totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
-  }
+    totalPages: Math.ceil(postsWithTags.length / POSTS_PER_PAGE),
+  };
 
   return {
     props: {
-      posts,
+      posts: postsWithTags,
       initialDisplayPosts,
       pagination,
     },
-  }
+  };
 }
 
 export default function PostPage({ posts, initialDisplayPosts, pagination }) {
   return (
     <>
-      <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
+      <PageSEO
+        title={siteMetadata.title}
+        description={siteMetadata.description}
+      />
       <ListLayout
         posts={posts}
         initialDisplayPosts={initialDisplayPosts}
@@ -52,5 +73,5 @@ export default function PostPage({ posts, initialDisplayPosts, pagination }) {
         title="All Posts"
       />
     </>
-  )
+  );
 }
